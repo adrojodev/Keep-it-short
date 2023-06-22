811394563f378696beb500a5ae5e54b35b583ee5
David Hernández <rojohernandez75@gmail.com>
2023-06-22 14:42:52 -0600

    Added metatags

--- app/page.tsx
+++ app/page.tsx
@@ -43,6 +43,11 @@ export default function Home() {
       weatherValue.humidity
     );
 
+    if (response === false) {
+      setShorts(undefined);
+      return;
+    }
+
     if (response === "yes") {
       setShorts(true);
     } else {
