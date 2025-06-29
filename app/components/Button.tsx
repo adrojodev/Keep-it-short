d624bd3f6a9683c300e076e25f1115f54e953768
David Hernández <50053027+adrojodev@users.noreply.github.com>
2025-06-29 06:11:15 -0600

    undo button

---
+++ app/components/Button.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import classNames from "classnames";
+import { ButtonHTMLAttributes } from "react";
+import { CircleNotch } from "@phosphor-icons/react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  isLoading?: boolean;
+}
+
+const Button = ({ isLoading, children, ...props }: ButtonProps) => {
+  return (
+    <button
+      disabled={isLoading}
+      {...props}
+      className={classNames(
+        "bg-neutral-100 px-4 py-2 text-black w-fit rounded-full relative flex justify-center items-center disabled:opacity-75 enabled:hover:bg-neutral-50 enabled:hover:scale-105 transition-all",
+        props.className
+      )}
+    >
+      {isLoading && <CircleNotch className="animate-spin absolute" />}
+      <span className={classNames(isLoading && "text-transparent")}>
+        {children}
+      </span>
+    </button>
+  );
+};
+
+export default Button;
