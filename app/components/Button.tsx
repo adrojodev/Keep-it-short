aec5767b16df7df7f33fd29803f12272c3454b09
David Hernández <50053027+adrojodev@users.noreply.github.com>
2025-07-01 10:10:15 -0600

    Fix hover button on darkmode

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -14,7 +14,7 @@ const Button = ({ isLoading, children, ...props }: ButtonProps) => {
       disabled={isLoading}
       {...props}
       className={classNames(
-        "bg-neutral-100 border border-neutral-400 dark:border-neutral-600 dark:bg-neutral-900 px-4 py-2 text-neutral-950 dark:text-neutral-50 w-fit rounded-full relative flex justify-center items-center disabled:opacity-75 enabled:hover:bg-neutral-50 enabled:hover:scale-105 transition-all",
+        "bg-neutral-100 border dark:bg-neutral-900 border-neutral-400 dark:border-neutral-600 px-4 py-2 text-neutral-950 dark:text-neutral-50 w-fit rounded-full relative flex justify-center items-center disabled:opacity-75 enabled:hover:bg-neutral-50 dark:enabled:hover:bg-neutral-950 enabled:hover:scale-105 transition-all",
         props.className
       )}
     >
